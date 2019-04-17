fe277ae0dd4bbbbeb1cdb87eada21262bf1adde6
Jang Whemoon <palindrom615@gmail.com>
2019-04-17 18:04:37 +0900

    Write skeleton code for test

---
+++ src/test/scala/graph/GraphSpec.scala
@@ -0,0 +1,17 @@
+package graph
+import org.scalatest._
+
+class GraphSpec extends FunSpec with Matchers {
+  describe("Graphs") {
+    it("P80 (***) Conversions.") {}
+    it("P81 (**) Path from one node to another one.") {}
+    it("P82 (*) Cycle from a given node.") {}
+    it("P83 (**) Construct all spanning trees.") {}
+    it("P84 (**) Construct the minimal spanning tree.") {}
+    it("P85 (**) Graph isomorphism.") {}
+    it("P86 (**) Node degree and graph coloration.") {}
+    it("P87 (**) Depth-first order graph traversal.") {}
+    it("P88 (**) Connected components.") {}
+    it("P89 (**) Bipartite graphs.") {}
+  }
+}
---
+++ src/test/scala/miscellaneous/MiscellaneousProblemSpec.scala
@@ -0,0 +1,17 @@
+package miscellaneous
+import org.scalatest._
+
+class MiscellaneousProblemSpec extends FunSpec with Matchers {
+  describe("Miscellaneous Problems") {
+    it("P90 (**) Eight queens problem") {}
+    it("P91 (**) Knight's tour.") {}
+    it("P92 (***) Von Koch's conjecture.") {}
+    it("P93 (***) An arithmetic puzzle.") {}
+    it("P94 (***) Generate K-regular simple graphs with N nodes.") {}
+    it("P95 (**) English number words.") {}
+    it("P96 (**) Syntax checker.") {}
+    it("P97 (**) Sudoku. (alternate solution)") {}
+    it("P98 (***) Nonograms.") {}
+    it("P99 (***) Crossword puzzle.") {}
+  }
+}
